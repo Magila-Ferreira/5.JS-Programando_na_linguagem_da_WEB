ce28e991d323938d1ba0d342683c2ca8dff55bf8
Mágila-Ferreira <magilamorganasf@hotmail.com>
2021-05-31 11:00:29 -0300

    Adicionando funções com o JS

--- js/principal.js
+++ js/principal.js
@@ -65,4 +65,13 @@ for (var i = 0; i < pacientes.length; i++) {
 
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("Botão clicado!")
+})
+
+
+
 
