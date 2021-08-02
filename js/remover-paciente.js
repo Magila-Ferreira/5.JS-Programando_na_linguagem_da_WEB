025dc9d10db4dc825422b1316009b7325f2290e6
Mágila-Ferreira <magilamorganasf@hotmail.com>
2021-08-02 11:37:22 -0300

    02/08 - Aula 7 - JS: remoção, delegação e animação

---
+++ js/remover-paciente.js
@@ -0,0 +1,17 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("tbody");
+
+tabela.addEventListener("dblclick", function(event) {
+    var alvoEvento = event.target;          // td
+    var paiDoAlvo = alvoEvento.parentNode;  // tr
+
+    paiDoAlvo.classList.add("fadeOut");
+    
+    setTimeout(function () {
+        paiDoAlvo.remove();
+    }, 1000);     
+});
+
+
+
